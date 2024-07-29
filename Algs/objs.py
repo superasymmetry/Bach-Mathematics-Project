ada022cbcd6b3c4b5d479ca5e094b91784be1402
superasymmetry <cokeymint@gmail.com>
2024-07-28 20:28:22 -0400

    added more to object methods and main

--- Algs/objs.py
+++ Algs/objs.py
@@ -1,16 +1,34 @@
 import winsound
 
-class note:
+class Note:
     def __init__(self, pitch):
         self.pitch = pitch
 
     def play(self, duration):
         winsound.Beep(self.pitch, duration)
 
-class phrase:
-    def __init__(self, notes):
-        self.notes = []
+class Phrase:
+    def __init__(self, notes=None):
+        self.notes = notes if notes is not None else []
 
-    def play(self):
-        for i in self.notes:
-            i.play(self)
+    def play(self, duration=500):  # Default duration of 500ms for each note
+        for note in self.notes:
+            note.play(duration)
+
+    def inversion(self):
+        inverted_notes = [Note((12 - note.pitch) % 12) for note in self.notes]
+        return Phrase(inverted_notes)
+    
+    def transpose(self, interval):
+        transposed_notes = [Note((note.pitch + interval) % 12) for note in self.notes]
+        return Phrase(transposed_notes)
+    
+    def retrograde(self):
+        retrograded_notes = self.notes[::-1]
+        return Phrase(retrograded_notes)
+
+    def retrograde_inversion(self):
+        return self.retrograde().inversion()
+
+    def add_note(self, note):
+        self.notes.append(note)
--- main.py
+++ main.py
@@ -1,2 +1,34 @@
 import random
+from objs import *
 
+if __name__ == "__main__":
+    # Create an empty phrase
+    phrase = Phrase()
+
+    # Add notes to the phrase
+    note1 = Note(60)  # C4
+    note2 = Note(62)  # D4
+    note3 = Note(64)  # E4
+
+    phrase.add_note(note1)
+    phrase.add_note(note2)
+    phrase.add_note(note3)
+
+    # Play the phrase
+    phrase.play()
+
+    # Transpose the phrase and play
+    transposed_phrase = phrase.transpose(2)
+    transposed_phrase.play()
+
+    # Invert the phrase and play
+    inverted_phrase = phrase.inversion()
+    inverted_phrase.play()
+
+    # Retrograde the phrase and play
+    retrograded_phrase = phrase.retrograde()
+    retrograded_phrase.play()
+
+    # Retrograde inversion and play
+    retrograded_inverted_phrase = phrase.retrograde_inversion()
+    retrograded_inverted_phrase.play()
